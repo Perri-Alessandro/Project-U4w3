c6160303e1cb63bbd7b977e0464c349e1538ae2e
Perri-Alessandro <63149331+Perri-Alessandro@users.noreply.github.com>
2024-03-22 17:00:20 +0100

    PrestitoDAO fixed

--- src/main/java/perri/dao/PrestitoDAO.java
+++ src/main/java/perri/dao/PrestitoDAO.java
@@ -2,12 +2,8 @@ package perri.dao;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
-import jakarta.persistence.TypedQuery;
 import perri.entities.Prestito;
-import perri.entities.Utente;
 import perri.exceptions.NotFoundException;
-
-import java.util.List;
 
 public class PrestitoDAO {
 
@@ -30,15 +26,15 @@ public class PrestitoDAO {
     }
 
 
-    public Utente getByNumeroTessera(long numeroTessera) {
-        Utente elemento = em.find(Utente.class, numeroTessera);
-        if (elemento == null) throw new NotFoundException(numeroTessera);
+    public Prestito getById(long id) {
+        Prestito elemento = em.find(Prestito.class, id);
+        if (elemento == null) throw new NotFoundException(id);
         return elemento;
     }
 
-    public void delete(long numeroTessera) {
+    public void delete(long id) {
         try {
-            Utente trovato = this.getByNumeroTessera(numeroTessera);
+            Prestito trovato = this.getById(id);
 
             EntityTransaction tx = em.getTransaction();
 
@@ -48,18 +44,9 @@ public class PrestitoDAO {
 
             tx.commit();
 
-            System.out.println("UTENTE " + trovato.getName() + " ELIMINATO CON SUCCESSO");
+            System.out.println("PRESTITO DI UTENTE " + trovato.getUtente().getName() + " ELIMINATO CON SUCCESSO");
         } catch (NotFoundException e) {
-            System.err.println("ERRORE NEL CANCELLARE L'UTENTE: " + e.getMessage());
-        }
-    }
-
-
-    public void visualizzaUtenti() {
-        TypedQuery<Utente> query = em.createQuery("SELECT u FROM Utente u", Utente.class);
-        List<Utente> risultati = query.getResultList();
-        for (Utente c : risultati) {
-            System.out.println(c);
+            System.err.println("ERRORE NEL CANCELLARE IL PRESTITO DI UTENTE : " + e.getMessage());
         }
     }
 }
